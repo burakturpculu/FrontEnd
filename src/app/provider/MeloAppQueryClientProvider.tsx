0bf5a6e4771e4d113d5d51c8121d81a25c412b0f
Burak Turpculu <ismailburakturpculu@gmail.com>
2023-09-09 12:19:04 +0300

    fixed toast error handle

--- src/app/provider/MeloAppQueryClientProvider.tsx
+++ src/app/provider/MeloAppQueryClientProvider.tsx
@@ -1,16 +1,59 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  MutationCache,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { AxiosError } from "axios";
 import { useState } from "react";
+import toast, { ToastOptions } from "react-hot-toast";
 
 export default function MeloAppQueryClientProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const mutationCache = new MutationCache({
+    onError: (error, _variables, _context, mutation) => {
+      if (mutation.options.onError) {
+        return;
+      }
+
+      const commonToastOptions: ToastOptions = {
+        duration: 5000,
+        position: "top-right",
+      };
+
+      if (error instanceof AxiosError) {
+        if (error.response?.data?.exception?.message) {
+          toast.error(`${error.response.data.exception?.message}`, commonToastOptions);
+          return;
+        }
+
+        if (error.response?.data?.resourceCode) {
+          toast.error(
+            `${error.response.data.resourceCode}`,
+            commonToastOptions
+          );
+          return;
+        }
+
+        if (error.response?.data?.messages?.length) {
+          error.response.data.messages.forEach((message: string) => {
+            toast.error(`errors.${message}`, commonToastOptions);
+          });
+          return;
+        }
+      }
+
+      toast.error("errors.unknown", commonToastOptions);
+    },
+  });
   const [queryClient] = useState(
     () =>
       new QueryClient({
+        mutationCache,
         defaultOptions: {
           queries: {
             suspense: true,
